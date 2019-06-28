3ea255c1c4247d28f964722e7059ca77d3d6c882
DenisSch <DenisSch@users.noreply.github.com>
2019-06-28 16:33:00 -0400

    clear get_mean variable

--- histoCAT/Loading_New/DataProcessing/Get_mean_batch.m
+++ histoCAT/Loading_New/DataProcessing/Get_mean_batch.m
@@ -6,6 +6,8 @@ function [get_mean,get_mean_name] = Get_mean_batch(marker_position,...
 % Load session
 load(sessionData_name)
 
+clear get_mean
+
 % Get global variables
 global samplefolders
 global Mask_all
@@ -16,7 +18,7 @@ Current_Mask = Mask_all.Image;
 % get mean expression for multipage tiff
 global tiff_name
 large_tiff_location = fullfile(samplefolders{1,1},tiff_name);
-get_mean = struct2array(regionprops(Current_Mask, ...
+    get_mean = struct2array(regionprops(Current_Mask, ...
     imread(large_tiff_location,marker_position), 'MeanIntensity'))';
 %Check to make sure it is a string
 get_mean_name_onlyname = (table2cell(Marker_list(marker_position,1)));
