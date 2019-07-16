a77713b5c0dbe32f6c582e0288a04eb2c25409f6
Edik Novik <ediknovik@Ediks-MacBook-Pro.local>
2019-07-16 11:40:28 -0400

    wip

--- call_33466POST_Headless_histoCAT_loading.m
+++ call_33466POST_Headless_histoCAT_loading.m
@@ -1,7 +1,7 @@
 clear all
 clc
 
-addpath(genpath('/home/en100/histoCAT'))
+%addpath(genpath('/home/en100/histoCAT'))
 
 %Specify actual parameters to pass to Headless_histoCAT_loading.m
 samplefolders_str = '/home/en100/Images/';
