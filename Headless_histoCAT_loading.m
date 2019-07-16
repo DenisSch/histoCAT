dcab58927af8c161e3145d77be2db3aabb77e4f2
Edik Novik <ediknovik@Ediks-MacBook-Pro.local>
2019-07-16 13:07:46 -0400

    include AF correlation code here

--- Headless_histoCAT_loading.m
+++ Headless_histoCAT_loading.m
@@ -171,29 +171,129 @@ disp('save CSV')
 writetable(Fcs_Interest_all{1,1},...
     fullfile(sessionData_mean_folder, strcat(tiff_name_raw{1,1},'.csv')));
 
-% %% Run AF correlation
-% %Define path where marker mean .mat files are saved
-% mean_path = strcat(samplefolders_str,'mean_output/Example/');
-% exampleCSV = readtable(fullfile(mean_path, 'Example.csv'));
-% 
-% %Extract marker names
-% Marker_list = table2array(readtable(Marker_CSV,'ReadVariableNames',false));
-% numMarkers = length(Marker_list);
-% 
-% %Save pixel arrays for each cell across markers
-% pixels_across_markers = {};
-% 
-% pixel_path = strcat(samplefolders_str,'pixel_output/Example/');
-% 
-% for i = 1:numMarkers
-%     tmp = load(strcat(pixel_path,'Cell_Example',Marker_list{i,1},'.mat'),'get_pixels');
-%     pixels_across_markers{i} = tmp.get_pixels;
-% end
-% 
-% save('pixels_across_markers.mat','pixels_across_markers','-v7.3');
-% disp('saved patient pixels');
-% 
-% %load(pixels_across_markers
+%% Run AF correlation
+disp('ran histoCAT!')
+
+%Define path where marker mean .mat files are saved
+% mean_path = strcat(samplefolders_str,'mean_output/33466POST/');
+% CSV_33466POST= readtable(fullfile(mean_path, '33466POST.csv'));
+mean_path = '/home/en100/Pixel_Correlation_Results/mean_output/Example/';
+csv_file = readtable(fullfile(mean_path, 'Example.csv'));
+
+%Extract marker names
+Marker_list = table2array(readtable(Marker_CSV,'ReadVariableNames',false));
+numMarkers = length(Marker_list);
+
+%Save pixel arrays for each cell across markers
+pixels_across_markers = {};
+
+% pixel_path = strcat(samplefolders_str,'pixel_output/33466POST/');
+pixel_path = '/home/en100/Pixel_Correlation_Results/pixel_output/Example/';
+
+for i = 1:numMarkers
+%     marker_pixels = load(strcat(pixel_path,'Cell_33466POST',Marker_list{i,1},'.mat'),'get_pixels');
+    marker_pixels = load(strcat(pixel_path,'Cell_Example',Marker_list{i,1},'.mat'),'get_pixels');
+    %% Filter out cells that have 10 pixels or less
+    marker_pixels_filtered = marker_pixels;
+    
+    pixel_lengths = [];
+    for k = 1:length(marker_pixels_filtered.get_pixels)
+        pixel_lengths(k) = length(marker_pixels_filtered.get_pixels{k,1});
+    end
+    pixel_lengths = pixel_lengths';
+    cells_to_filter = pixel_lengths <= 50;
+    
+    marker_pixels_filtered.get_pixels(cells_to_filter) = [];
+    %%
+    pixels_across_markers{i} = marker_pixels_filtered.get_pixels;
+end
+
+% % % save('pixels_across_markers.mat','33466_POST_pixels_across_markers','-v7.3');
+% % % disp('saved patient pixels');
+
+numCells = size(pixels_across_markers{1,1},1);
+
+%Perform pixel correlations for each cell across markers, pairwise
+corrs = zeros(numCells,4,4);
+pears_corrs = {}; %Save pixel correlations here
+
+tic
+%Choose which marker channels to include for correlation matrices
+%AF channels:
+n_start = 1; 
+n_end = 4;
+
+%Cycle 1 channels (including Hoechst):
+m_start = 5;
+m_end = 8;
+
+for j = 1:numCells %for each cell
+    %two for loops for pairwise correlations across markers
+    %find correlations of AF markers and cycle 1
+    for n = n_start:n_end 
+        for m = m_start:m_end
+            
+            cell_j_marker_n = double(pixels_across_markers{1,n}{j,1});
+            cell_j_marker_m = double(pixels_across_markers{1,m}{j,1});
+            
+            pearson_corr = corrcoef(cell_j_marker_n, cell_j_marker_m);
+            %extract correlation coefficient
+            R = pearson_corr(1,2); %either off-diagonal (1,2) or (2,1) works
+            
+            %save correlation coefficient in matrix
+            corrs(j,n-n_start+1,m-m_start+1) = R;
+        end
+    end
+    %Reshape to 2D 3x3 matrix 
+    %pears_corrs{j} = reshape(corrs(j,:,:),[4,4])';
+    pears_corrs{j} = reshape(corrs(j,:,:),[4,4]);
+end
+toc
+
+%Plot single cells with correlation overlay 
+corr_label = [];
+for k = 1:size(pears_corrs,2)
+    corr_label = vertcat(corr_label,diag(pears_corrs{k})');
+end
+
+tiff_name_raw = strsplit(tiff_name,'.');
+
+f1 = figure('visible', 'off');
+scatter(csv_file.X_position(~cells_to_filter),csv_file.Y_position(~cells_to_filter),[],corr_label(:,1), 'filled')
+colorbar
+caxis([-1 1])
+title(strcat('Pixel Pearson Correlation Across Cells (Hoechst1-Hoechst2)'))
+saveas(gcf,strcat(tiff_name_raw{1,1},'-Hoechst1-Hoechst2-correlation','.fig'))
+saveas(gcf,strcat(tiff_name_raw{1,1},'-Hoechst1-Hoechst2-correlation','.tif')) 
+close(f1)
+
+f2 = figure('visible', 'off');
+scatter(csv_file.X_position(~cells_to_filter),csv_file.Y_position(~cells_to_filter),[],corr_label(:,2), 'filled')
+colorbar
+caxis([-1 1])
+title(strcat('Pixel Pearson Correlation Across Cells (A488-pERK)'))
+saveas(gcf,strcat(tiff_name_raw{1,1},'-A488-pERK-correlation','.fig'))
+saveas(gcf,strcat(tiff_name_raw{1,1},'-A488-pERK-correlation','.tif')) 
+close(f2)
+
+f3 = figure('visible', 'off');
+scatter(csv_file.X_position(~cells_to_filter),csv_file.Y_position(~cells_to_filter),[],corr_label(:,3), 'filled')
+colorbar
+caxis([-1 1])
+title(strcat('Pixel Pearson Correlation Across Cells (A555-AXL)'))
+saveas(gcf,strcat(tiff_name_raw{1,1},'-A555-AXL-correlation','.fig'))
+saveas(gcf,strcat(tiff_name_raw{1,1},'-A555-AXL-correlation','.tif')) 
+close(f3)
+
+f4 = figure('visible', 'off');
+scatter(csv_file.X_position(~cells_to_filter),csv_file.Y_position(~cells_to_filter),[],corr_label(:,4), 'filled')
+colorbar
+caxis([-1 1])
+title(strcat('Pixel Pearson Correlation Across Cells (A647-MITF)'))
+saveas(gcf,strcat(tiff_name_raw{1,1},'-A647-MITF-correlation','.fig'))
+saveas(gcf,strcat(tiff_name_raw{1,1},'-A647-MITF-correlation','.tif')) 
+close(f4)
+
 
 end
 
